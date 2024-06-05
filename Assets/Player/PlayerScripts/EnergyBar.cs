6d26d7e4dc6ce55feedb13bc14cff0976f23bf0e
Tseo232 <113905825+Tseo232@users.noreply.github.com>
2024-06-05 19:30:59 +0200

    just bare with me

--- Assets/Player/PlayerScripts/EnergyBar.cs
+++ Assets/Player/PlayerScripts/EnergyBar.cs
@@ -1,26 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class EnergyBar : MonoBehaviour
-{
-public Slider EBslider;
-	//public Gradient gradient;
-	public Image fill;
+// public class EnergyBar : MonoBehaviour
+// {
+// public Slider EBslider;
+// 	//public Gradient gradient;
+// 	public Image fill;
 
-	public void SetMaxEnergy(float energy)
-	{
-		EBslider.maxValue = energy;
-		EBslider.value = energy;
+// 	public void SetMaxEnergy(float energy)
+// 	{
+// 		EBslider.maxValue = energy;
+// 		EBslider.value = energy;
 
-		//fill.color = gradient.Evaluate(1f);
-	}
+// 		//fill.color = gradient.Evaluate(1f);
+// 	}
 
-    public void SetEnergy(float energy)
-	{
-		EBslider.value = energy;
+//     public void SetEnergy(float energy)
+// 	{
+// 		EBslider.value = energy;
 
-		//fill.color = gradient.Evaluate(slider.normalizedValue);
-	}
-}
+// 		//fill.color = gradient.Evaluate(slider.normalizedValue);
+// 	}
+// }
 
